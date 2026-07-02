7958c396584ec3e50e342166d3e3977dc91ce690
Wanzetse <wanzie.ke@gmail.com>
2026-07-02 15:28:35 +0300

    error handling

--- main.py
+++ main.py
@@ -14,17 +14,36 @@ def main():
   4. Load: push to MySQL
   """
   
-  df = load_data(csv_path)
-  print(f'Loaded {len(df)} rows.')
-
-  df = clean_data(df)
-
-  passed = run_all_checks(df)
-  if not passed:
-    print('Quality checks failed. Stopping pipeline')
+  try:
+    df = load_data(csv_path)
+    print(f'Loaded {len(df)} rows.')
+  except FileNotFoundError:
+    print(f'Error: CSV file not found at {csv_path}. Please check the path')
+    return
+  except Exception as e:
+    print('Error loading data: {e}}')
+    return
+ 
+  try:
+    df = clean_data(df)
+  except Exception as e:
+    print(f'Error cleaning data: {e}')
     return 
 
-  load_to_mysql(df)
+  try:
+    passed = run_all_checks(df)
+    if not passed:
+      print('Quality checks failed. Stopping pipeline')
+      return 
+  except Exception as e:
+    print(f'Error during quality checks: {e}')
+    return
+
+  try:
+    load_to_mysql(df)
+  except Exception as e:
+    print(f'Error loading to MySQL: {e}')
+    return
 
 if __name__ == '__main__':
   main()
